cd501b48706df1e857ce926e93f302a608dea05e
Nicolas Schmid <nicolas@breiten.ch>
2025-01-13 11:49:52 +0100

    feat: improve log handling

--- hermes_model/logging.py
+++ hermes_model/logging.py
@@ -1,15 +1,29 @@
-try:
-    from prefect.logging import get_run_logger
-    PREFECT = True
-except ImportError:
-    import logging
-    PREFECT = False
+import logging
 
 
-def get_logger():
-    if PREFECT:
-        logger = get_run_logger()
-    else:
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logging.INFO)
-    return logger
+def set_up_logging(module_name):
+    """
+    If Prefect is installed, set up logging for the given module name.
+
+    The standard log is "redirected" by prefect, therefore set up an
+    additional handler for the given module.
+    """
+    try:
+        from prefect.logging import get_logger  # noqa
+    except ImportError:
+        return
+
+    module_name = module_name.split(".")[0]
+
+    root_logger = logging.getLogger(module_name)
+
+    handler = logging.StreamHandler()
+
+    handler.setFormatter(logging.Formatter(
+        "%(asctime)s.%(msecs)03d | %(levelname)-7s | "
+        "%(name)r - %(message)s",
+        datefmt="%H:%M:%S"))
+
+    root_logger.addHandler(handler)
+
+    root_logger.setLevel(logging.INFO)
